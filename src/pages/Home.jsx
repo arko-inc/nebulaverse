acdb5257fa1e99cc4b6e46f6e2d2045765be5560
Arko <arkotesla@gmail.com>
2024-12-06 19:16:28 +0600

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useUser } from "../lib/context/user";
+import { useUser} from "../lib/context/user";
 import { useIdeas } from "../lib/context/Ideas";
 
 export function Home() {
